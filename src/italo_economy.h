434c36e72b97e2e701e48acc21124bdab9371c4a
italo <info@italo.network>
2020-11-29 15:57:46 +0000

    try emission supply

--- src/italo_economy.h
+++ src/italo_economy.h
@@ -4,7 +4,7 @@
 constexpr uint64_t COIN                       = (uint64_t)1000000000; // 1 ITALO = pow(10, 9)
 constexpr uint64_t MONEY_SUPPLY               = ((uint64_t)(-1)); // MONEY_SUPPLY - total number coins to be generated
 constexpr uint64_t EMISSION_LINEAR_BASE       = ((uint64_t)(1) << 58);
-constexpr uint64_t EMISSION_SUPPLY_MULTIPLIER = 19;
+constexpr uint64_t EMISSION_SUPPLY_MULTIPLIER = 24;
 constexpr uint64_t EMISSION_SUPPLY_DIVISOR    = 10;
 constexpr uint64_t EMISSION_DIVISOR           = 2000000;
 
