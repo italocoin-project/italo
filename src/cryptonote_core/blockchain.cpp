5a47daa0e59514b51980530d6a3ab092abce6225
italo <info@italo.network>
2020-11-29 15:47:00 +0000

    remove diff divergents

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -4010,18 +4010,6 @@ Blockchain::block_pow_verified Blockchain::verify_block_pow(cryptonote::block co
   crypto::hash const blk_hash = cryptonote::get_block_hash(blk);
   uint64_t const blk_height   = cryptonote::get_block_height(blk);
 
-  // There is a difficulty bug in italod that caused a network disagreement at height 526483 where
-  // somewhere around half the network had a slightly-too-high difficulty value and accepted the
-  // block while nodes with the correct difficulty value rejected it.  However this not-quite-enough
-  // difficulty chain had enough of the network following it that it got checkpointed several times
-  // and so cannot be rolled back.
-  //
-  // Hence this hack: starting at that block until the next hard fork, we allow a slight grace
-  // (0.2%) on the required difficulty (but we don't *change* the actual difficulty value used for
-  // diff calculation).
-  if (cryptonote::get_block_height(blk) >= 526483 && m_hardfork->get_current_version() < network_version_16_pulse)
-    difficulty = (difficulty * 998) / 1000;
-
   CHECK_AND_ASSERT_MES(difficulty, result, "!!!!!!!!! difficulty overhead !!!!!!!!!");
   if (alt_block)
   {
