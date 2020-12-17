4f04066dfce6b7fcdafa0fad886d1c0f613a90ff
italo <info@italo.network>
2020-12-17 10:24:32 +0000

    respawn HF

--- src/cryptonote_basic/hardfork.cpp
+++ src/cryptonote_basic/hardfork.cpp
@@ -66,9 +66,8 @@ static uint8_t get_block_version(const cryptonote::block &b)
 static constexpr HardFork::Params mainnet_hard_forks[] =
 {
   { network_version_7,                      1, 0, 1605737882 },
-  { network_version_15_lns,                 120, 0, 1605737882 + 10*60}, // 2020-03-25 14:00AEDT (03:00UTC)
-  { network_version_16_pulse,               300, 0, 1605737882 + 20*60}, // 2020-10-12 12:00AEDT (01:00UTC)
-  { network_version_17,                     300 + BLOCKS_EXPECTED_IN_DAYS(180), 0, 1605737882 + 180*24*60*60 }, // HF16 + 180 days
+  { network_version_16_pulse,               120, 0, 1605737882 + 10*60}, // 2020-03-25 14:00AEDT (03:00UTC)
+  { network_version_17,                     120 + BLOCKS_EXPECTED_IN_DAYS(180), 0, 1605737882 + 180*24*60*60 }, // HF16 + 180 days
 };
 
 static constexpr HardFork::Params testnet_hard_forks[] =
