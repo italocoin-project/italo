94e724bfbecc6f847c0e60233ae70a6f1278b271
italo <info@italo.network>
2021-03-18 13:50:43 +0000

    Bump Version to 10.0.0 , Set Hardfork
    Harfork will happen around 3/22/2021

--- src/cryptonote_basic/hardfork.cpp
+++ src/cryptonote_basic/hardfork.cpp
@@ -68,7 +68,8 @@ static constexpr HardFork::Params mainnet_hard_forks[] =
   { network_version_7,                      1, 0, 1605737882 },
   { network_version_11_infinite_staking,    10, 0, 1605737882  + 10*60}, // 2019-03-26 13:00AEDT
   { network_version_16_pulse,               120, 0, 1605737882 + 20*60}, // 2020-03-25 14:00AEDT (03:00UTC)
-  { network_version_17,                     120 + BLOCKS_EXPECTED_IN_DAYS(180), 0, 1605737882 + 180*24*60*60 }, // HF16 + 180 days
+  { network_version_17,                     130000 + BLOCKS_EXPECTED_IN_DAYS(4), 0, 1616074619 + 4*24*60*60 }, 
+  { network_version_18,                     130000 + BLOCKS_EXPECTED_IN_DAYS(4) + 1, 0, 1616074619 + 4*24*60*60 + 120}, 
 };
 
 static constexpr HardFork::Params testnet_hard_forks[] =
